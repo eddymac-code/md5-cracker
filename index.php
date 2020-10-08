f49565c1ec294acb42027f9a36697143ba3b4b1d
Eddy Mac <eddymac11@outlook.com>
2020-10-08 15:23:06 +0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>MD5 Cracker</title>
+</head>
+<body>
+<h1>I'll crack some MD5</h1>
+<p>This application takes an MD5 hash of a 4-digit pin,
+ checks the hash against 10,000 possible 4-digit pins, and
+ returns the correct pin</p>
+
+ <form method="GET">
+ <input type="text" name="md5" size="40">
+ <input type="submit" value="Crack MD5">
+ </form>
+</body>
+</html>
